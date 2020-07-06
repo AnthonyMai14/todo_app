f2978ea3a07eb7a4bd448f9016fa5177268cc7f0
Anthony Mai <anthonymai14@gmail.com>
2020-07-05 23:36:22 -0700

    Add check box to label

--- default/js/script.js
+++ default/js/script.js
@@ -3,6 +3,8 @@ var arrayTask = [];
 function Task(description, status) {
     this.description = description;
     this.status = status;
+
+    status = false;
 }
 
 $(function() {
@@ -18,6 +20,18 @@ $(function() {
     //     newTask.hide();
     // });
 
+    //only show false (i.e. active/incomplete) tasks
+    // $('#menu-active').click(function() {
+    //     $.each(arrayTask, function (index, description) {
+
+    //     });
+    // }));
+
+    //only show true (i.e. completed) tasks
+    // $('#menu-completed').click(function() {
+
+    // });
+
     //Add new task to list after keypress <ENTER> AND valid input
     $(document).keypress(function (event) {
         var keycode = (event.keyCode ? event.keyCode : event.which);
@@ -27,15 +41,15 @@ $(function() {
 
             var newTaskValue =  document.querySelector('#task-new-input').value;
             var numInArray = "arrNum" + (arrayTask.length);
-            arrayTask.push(new Task(newTaskValue, 'incomplete'));
+            arrayTask.push(new Task(newTaskValue, false));
 
             //hide input text
             newTask.hide();
             //set newTask to null
             newTask.value = "";
-
-            $('#task-exist').append(arrayTask[arrayTask.length-1].description);
-            $('#task-exist').append('<br>');
+            
+            var newTaskObj = arrayTask[arrayTask.length - 1];
+            $('#task-exist').append('<div class="task-wrapper"><input type="checkbox" class="incomplete" id="' + newTaskObj + '"><label for="' + newTaskObj + '">' + newTaskObj.description + '</label><br>');
         }
 
     });
