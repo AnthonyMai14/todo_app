5a36f146198fd1084d9bb2256d22d26d6996b313
Anthony Mai <anthonymai14@gmail.com>
2020-07-08 20:26:02 -0700

    Clear input text box after <ENTER>

--- default/js/script.js
+++ default/js/script.js
@@ -1,4 +1,5 @@
 var arrayTask = [];
+var tempHTML = "";
 
 function Task(description, status) {
     this.description = description;
@@ -6,6 +7,19 @@ function Task(description, status) {
 
     status = false;
 }
+
+// if (arrayTask.length !== 0) {
+//     $.each(arrayTask, function (indexObj) {
+//         var newTaskID = "arrNum" + indexObj;
+//         tempHTML = "bob";
+//         tempHTML = '<div class="task-wrapper' + newTaskID + '"><input type="checkbox" class="';
+//         tempHTML += (index.status === false) ? "incomplete " : "complete ";
+//         tempHTML += (newTaskID + '"><label for="' + newTaskID + '">' + indexObj.description + '</label><img src="default/img/trashIcon.png" class="task-delete"></div>');
+//         alert("tempHTML");
+
+//     });
+//     $('#task-exist').append(tempHTML);
+// }
 
 $(function() {
     var newTask = $('.task-new');
@@ -15,6 +29,14 @@ $(function() {
         newTask.show();
     }
     );
+
+    $('input[type=checkbox]checked').each(function() {
+        this.addClass('complete').removeClass('incomplete');
+    });
+
+    $('input[type=checkbox]unchekced').each(function () {
+        this.addClass('incomplete').removeClass('complete');
+    });
     
     //hide input text when click on <main> but not children of main
     $('main').click(function(e){
@@ -22,17 +44,30 @@ $(function() {
         newTask.hide();
     });
 
-    //iterate through #task-exist and only show false (i.e. active/incomplete) tasks
-    // $('#menu-active').click(function() {
-    //     $.each(arrayTask, function (index, description) {
+    //delete task
+    $('.task-wrapper > .task-delete').click(function(e) {
+        this.parentElement.remove();
+    });
+        //retrieve ID
+        //delete div
+        //delete that index from the array
 
-    //     });
-    // }));
+    $('#menu-all').click(function () {
+        $('.completed').parentElement.css('display', 'show');
+        $('.incomplete').parentElement.css('display', 'show');
+    });
+    
+    // iterate through #task-exist and only show false (i.e. active/incomplete) tasks
+    $('#menu-active').click(function() {
+        $('.completed').parentElement.css('display','hide');
+        $('.incomplete').parentElement.css('display', 'show');
+    });
 
     //iterate through #task-exist and only show true (i.e. completed) tasks
-    // $('#menu-completed').click(function() {
-
-    // });
+    $('#menu-completed').click(function() {
+        $('.completed').parentElement.css('display', 'show');
+        $('.incomplete').parentElement.css('display', 'hide');
+    });
 
     //Add new task to list after keypress <ENTER> AND valid input
     $(document).keypress(function (event) {
@@ -46,12 +81,15 @@ $(function() {
 
             //hide input text
             newTask.hide();
-            //set newTask to null
-            newTask.value = "";
+            //set .task-new to null
+            $('.task-new').val('');
             
             var newTaskID = "arrNum" + (arrayTask.length - 1);
             var newTaskObj = arrayTask[arrayTask.length - 1];
-            $('#task-exist').append('<div class="task-wrapper ' + newTaskID + '"><input type="checkbox" class="incomplete ' + newTaskID + '"><label for="' + newTaskID + '">' + newTaskObj.description + '</label><img src="default/img/trashIcon.png" class="task-delete">');
+            $('#task-exist').append('<div class="task-wrapper ' + newTaskID + '"><input type="checkbox" class="incomplete ' + newTaskID + '"><label for="' + newTaskID + '">' + newTaskObj.description + '</label><img src="default/img/trashIcon.png" class="task-delete ' + newTaskID + '">');
+
+            if (event.preventDefault) event.preventDefault(); // This should fix it
+            return false; // Just a workaround for old browsers
         }
 
     });
